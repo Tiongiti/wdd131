29ecfacd67ac74dfa9eda4a006cc7168643b4962
Tiongiti <ttioti2011@gmail.com>
2024-11-09 12:51:26 +1400

    update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,5 @@
 // Set the current year
-const currentYear = new Date().getFullYear();
-document.getElementById("currentyear").textContent = currentYear;
+document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 // Set the last modified date
-const lastModified = document.lastModified;
-document.getElementById("lastModified").textContent = `Last Updated: ${lastModified}`;
+document.getElementById("lastModified").textContent += document.lastModified;
